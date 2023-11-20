8ca66bd38e93e88412b32e739c95b1a8baa1c6c1
Cheng-Yuan-Lai <a186235@g,ail.com>
2023-11-21 00:27:12 +0800

    feat: install S3 SDK

--- server/app.js
+++ server/app.js
@@ -1,23 +1,22 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
 import morgan from 'morgan';
-import * as dotenv from 'dotenv';
 import path, { dirname } from 'path';
 import cookieParser from 'cookie-parser';
 import { fileURLToPath } from 'node:url';
 import swaggerUi from 'swagger-ui-express';
-import 'dotenv/config';
 import eventRouter from './routes/event.js';
 import sourceMapRouter from './routes/sourceMap.js';
 import userRouter from './routes/user.js';
 import projectRouter from './routes/project.js';
+import validateRouter from './routes/validation.js';
 
 // read json file
 const rawData = fs.readFileSync('swagger_output.json');
 const swaggerOutput = JSON.parse(rawData);
 
-// eslint-disable-next-line no-underscore-dangle
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
@@ -37,7 +36,7 @@ app.use(cookieParser());
 // static file
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/1.0', [eventRouter, sourceMapRouter, userRouter, projectRouter]);
+app.use('/api/1.0', [eventRouter, sourceMapRouter, userRouter, projectRouter, validateRouter]);
 
 app.use((err, req, res, next) => {
   const error = Object.assign(err);
