14fe90e9882a1fbbaeee7a60cbe7844117633c15
Cheng-Yuan-Lai <a186235@g,ail.com>
2023-12-11 21:00:05 +0800

    fix: route syntx bug

--- server/routes/sourceMap.js
+++ server/routes/sourceMap.js
@@ -11,7 +11,7 @@ import {
 
 const router = Router();
 
-router.route('/api/1.0/sourceMap').post([
+router.route('/sourceMap').post([
   uploadToMemory.single('map'),
   parseDSN,
   checkSourceMapExisted,
